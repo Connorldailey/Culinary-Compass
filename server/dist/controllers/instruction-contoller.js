7d9d049d847a785a582851c2b0f24cd32410ee26
CH258 <ch2953har@gmail.com>
2024-12-05 17:13:00 -0500

    add instructions route and controller

---
+++ server/dist/controllers/instruction-contoller.js
@@ -0,0 +1,21 @@
+import { Instruction } from '../models/instruction.js';
+export const getInstructions = async (req, res) => {
+    const { recipeId } = req.params; // Get the recipeId from the request parameters
+    if (!recipeId) {
+        return res.status(400).json({ message: 'recipeId is required.' });
+    }
+    try {
+        let instructions = await Instruction.findOne({ where: { recipeId } });
+        if (!instructions) {
+            return res.status(404).json({ message: 'Instructions not found.' });
+        }
+        return res.status(201).json({
+            message: 'GET instructions successful',
+            data: instructions,
+        });
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Failed to get instuctions.' });
+    }
+};
---
+++ server/dist/routes/api/instructions-routes.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import { getInstructions } from '../../controllers/instruction-contoller.js';
+const router = express.Router();
+router.get('/:recipeId', getInstructions);
+export { router as instructionRouter };
