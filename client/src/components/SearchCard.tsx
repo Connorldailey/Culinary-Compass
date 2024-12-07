720f19fc289f7fa9c3d83a473a4c67912488efce
Connor Dailey <connorldailey@gmail.com>
2024-12-07 16:33:04 -0500

    make cards responsive

--- client/src/components/SearchCard.tsx
+++ client/src/components/SearchCard.tsx
@@ -11,22 +11,22 @@ const SearchCard: React.FC<SearchCardProps> = ({ data, addToList, viewRecipe }) 
     return (
         <>
             {data.map((recipe, index) => (
-                <div key={index} className='recipe-card d-flex mb-3 bg-dark text-light'>
+                <div key={index} className='recipe-card d-flex flex-column flex-md-row mb-3 bg-dark text-light'>
                     <img 
                         src={recipe.image} 
-                        className='card-image' 
+                        className='card-image img-fluid' 
                         alt={recipe.title} 
                         onError={(e) => {
                             e.currentTarget.src = noImage;
                         }}
                     />
-                    <div className='card-body p-3'>
-                        <h2 className="card-title">{recipe.title}</h2>
-                        <div className='d-flex'>
-                            <button className='btn' onClick={() => addToList(recipe, 'try-it')} aria-label="Try It">
+                    <div className='card-body d-flex flex-column p-3'>
+                        <h2 className="card-title text-center text-md-start p-3">{recipe.title}</h2>
+                        <div className='d-flex justify-content-center justify-content-md-around justify-content-lg-start'>
+                            <button className='btn pe-3' onClick={() => addToList(recipe, 'try-it')} aria-label="Try It">
                                 <i className='bi bi-bookmark-plus fs-1 text-light'></i>
                             </button>
-                            <button className='btn' onClick={() => addToList(recipe, 'favorite')} aria-label="Favorites">
+                            <button className='btn pe-3' onClick={() => addToList(recipe, 'favorite')} aria-label="Favorites">
                                 <i className='bi bi-heart fs-1 text-light'></i>
                             </button>
                             <button className='btn' onClick={() => viewRecipe(recipe.id)} aria-label="View Details">
